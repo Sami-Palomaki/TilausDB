b63267ee57a082dfae85ffdcf811ae8370cac61d
Sami1984Careeria <sami.palomaki@hotmail.com>
2021-12-19 15:56:50 +0200

    lisäyksiä

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TilausDBWebApp.Models;
 
 namespace TilausDBWebApp.Controllers
 {
@@ -10,21 +11,74 @@ namespace TilausDBWebApp.Controllers
     {
         public ActionResult Index()
         {
+            if (Session["UserName"] == null)
+            {
+                ViewBag.LoggedStatus = "Out";
+                return RedirectToAction("login", "home");
+            }
+            else ViewBag.LoggedStatus = "In";
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                ViewBag.Message = "Yhtiön Careeria tietoja.";
 
-            return View();
+                return View();
+            }
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                ViewBag.Message = "Yhteystietoja.";
+                ViewBag.UserName = Session["UserName"];
+                return View();
+            }
+        }
 
+        public ActionResult Login()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorize(Logins LoginModel)
+        {
+            TilausDBEntities db = new TilausDBEntities();
+            //Haetaan käyttäjän/Loginin tiedot annetuilla tunnustiedoilla tietokannasta LINQ -kyselyllä
+            var LoggedUser = db.Logins.SingleOrDefault(x => x.UserName == LoginModel.UserName && x.PassWord == LoginModel.PassWord);
+            if (LoggedUser != null)
+            {
+                ViewBag.LoginMessage = "Successfull login";
+                ViewBag.LoggedStatus = "In";
+                Session["UserName"] = LoggedUser.UserName;  // Perustetaan user-name sessio
+                return RedirectToAction("Index", "Home"); //Tässä määritellään mihin onnistunut kirjautuminen johtaa --> Home/Index
+            }
+            else
+            {
+                ViewBag.LoginMessage = "Login unsuccessfull";
+                ViewBag.LoggedStatus = "Out";
+                LoginModel.LoginErrorMessage = "Tuntematon käyttäjätunnus tai salasana.";
+                return View("Login", LoginModel);
+            }
+        }
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            ViewBag.LoggedStatus = "Out";
+            return RedirectToAction("Index", "Home"); //Uloskirjautumisen jälkeen pääsivulle
         }
     }
 }

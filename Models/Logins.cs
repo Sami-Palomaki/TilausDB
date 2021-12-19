0e47f51359ae9ab72f2dfc753630b5ed8f1a7c84
Sami1984Careeria <sami.palomaki@hotmail.com>
2021-12-19 16:13:12 +0200

    lisäyksiä

--- Models/Logins.cs
+++ Models/Logins.cs
@@ -16,8 +16,10 @@ namespace TilausDBWebApp.Models
     public partial class Logins
     {
         public int LoginId { get; set; }
+        [Required(ErrorMessage = "Anna käyttäjätunnus!")]
         public string UserName { get; set; }
         [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Anna salasana!")]
         public string PassWord { get; set; }
         public string LoginErrorMessage { get; set; }
     }
